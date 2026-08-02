39f037d481c7d3e802c01cc96ed8e9d24e74ac9e
Ryan_Fanart <nphoenix.art@gmail.com>
2026-08-01 22:42:34 -0300

    Ajuste do conteiner

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toggleMode() {
   const html = document.documentElement /* Representação do nosso documento*/
   html.classList.toggle("light")
+
   //pegar a tag img
   const img = document.querySelector("#profile img")
+
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se estiver light mode, adicionar a imagem light
